0a2b8f061c5cfce47b5b3b5e7e9a416e454ab2ea
OlivierMichaud1 <olivier.michaud2@mail.mcgill.ca>
2025-12-14 13:15:26 -0500

    fix: remove emergency storage set up due to compression issues

--- admin/admin_setup.sh
+++ admin/admin_setup.sh
@@ -10,15 +10,3 @@ git clone ${COMP597_REPO_URL}
 
 mkdir -p ${COMP597_PIP_CACHE}
 
-if [[ ${COMP597_EMERGENCY_STORAGE_ENABLED} = true ]]; then
-
-        mkdir -p ${COMP597_EMERGENCY_STORAGE_DIR}
-
-        cd ${COMP597_EMERGENCY_STORAGE_DIR}
-
-        for ((i=0; i<${COMP597_EMERGENCY_STORAGE_NB_BLOCKS}; i++)); do
-                # Unfortunately, it seems that the nfs partition uses a version of nfs that is too old to use fallocate, which would be much faster.
-                dd if=/dev/zero of=block-$i  bs=${COMP597_EMERGENCY_STORAGE_BLOCK_SIZE_IN_MB}  count=1
-        done
-fi
-
