2c45aec19782bb23d2bd029993e9e8df960c87b0
OlivierMichaud1 <olivier.michaud2@mail.mcgill.ca>
2025-12-15 12:05:57 -0500

    fix: provide output dir for codecarbon

--- src/config/trainer_stats/codecarbon/codecarbon_config.py
+++ src/config/trainer_stats/codecarbon/codecarbon_config.py
@@ -8,4 +8,5 @@ class TrainerStatsConfig(_BaseConfig):
         super().__init__()
         self._arg_run_num = _Arg(type=int, help="The run number used for codecarbon file tracking.", default=0)
         self._arg_project_name = _Arg(type=str, help="The name of the project used for codecarbon file tracking.", default="energy-efficiency")
+        self._arg_output_dir = _Arg(type=str, help="The path of the output directory where files will be saved.", default=".")
 
